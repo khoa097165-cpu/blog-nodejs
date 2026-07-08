6695213ca222a32a7dbb1744a95af802b96c588e
Nguyen Anh Khoa <khoa097165@gmail.com>
2026-07-08 08:41:56 +0700

    Khởi tạo dự án Express cơ bản

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('nguyen anh khoa');
+  res.send('nguyen anh khoa123');
 });
 app.get('/gioi-thieu', (req, res) => {
     res.send(`
@@ -22,6 +22,18 @@ app.get('/gioi-thieu', (req, res) => {
     </p> 
     `);
   });
+
+app.listen(port, () => {
+  console.log(`Server đang chạy tại http://localhost:${port}`);
+});
+const morgan = require('morgan'); 
+
+
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.send('Chào mừng đến với Blog cá nhân của tôi!');
+});
 
 app.listen(port, () => {
   console.log(`Server đang chạy tại http://localhost:${port}`);
