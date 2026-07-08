35a5679d186b8bf903a3d07968fa69c6a341a0a9
Nguyen Anh Khoa <khoa097165@gmail.com>
2026-07-08 08:19:39 +0700

    Khởi tạo dự án Express cơ bản

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('nguyen anh khoa');
+});
+app.get('/gioi-thieu', (req, res) => {
+    res.send(`
+    <h1>Thông tin cá nhân</h1>
+
+    <p>
+      <span><b>Họ và tên:</b> Nguyễn Anh Khoa</span> |
+      <span><b>Mã sinh viên:</b> 2606042025</span> |
+      <span><b>Lớp:</b> 20THL</span>
+    </p>
+
+    <p>
+      <span><b>Họ và tên:</b> Lê Nguyên Bảo Nam</span> |
+      <span><b>Mã sinh viên:</b> 2606042023</span> |
+      <span><b>Lớp:</b> 20THL</span>
+    </p> 
+    `);
+  });
+
+app.listen(port, () => {
+  console.log(`Server đang chạy tại http://localhost:${port}`);
+});
